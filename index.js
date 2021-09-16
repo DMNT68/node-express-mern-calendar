6929c02ed455a4213b51b9c07af27adb2b66d4b7
Andrés Salgado <andressalgado41@gmail.com>
2021-09-16 13:15:45 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('./database/config');
+require('dotenv').config();
+
+// Crear el servidor de express
+const app = express();
+
+// base de datos
+dbConnection();
+
+// Cors
+app.use(cors());
+
+// Directorio publico
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+
+// Escuchar peticiones
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+});
