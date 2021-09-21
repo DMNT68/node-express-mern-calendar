c819437c75487e832f2b8359fbdae85395a5bbab
Andrés Salgado <andressalgado41@gmail.com>
2021-09-20 22:38:58 -0500

    deploy heroku

--- database/config.js
+++ database/config.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(`${process.env.DB_CNN}`);
+    await mongoose.connect(process.env.DB_CNN);
 
     console.log('DB only');
   } catch (error) {
-    console.log(error);
+    console.log('-->',error);
     throw new 'Error a la hora de inicializar la base datos'();
   }
 };
